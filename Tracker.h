261af7c9de254af34ca9907cd7c0276f8b0d5b70
komplexon3 <mwidmer@protonmail.com>
2017-05-06 10:47:59 +0200

    added comments & removed BallBeamTest

--- Tracker.h
+++ Tracker.h
@@ -11,15 +11,28 @@ class Tracker {
   public:
     Tracker(int analogPin, int resistor, int sampleCount, int sampleSelect);
 
+    /*
+      analogPin: pin to which the voltage divider is connected
+      resistor: resistor used in voltage divider
+      sampleCount: number of measurements taken per reading
+      sampleSelect: number of lowest measurements of which the average is calculated
+    */
+
+    //read the resistance/position
+    //min/max resistance have to be set before position readings
     double readResistance();
     double readPosition();
 
+    //set min/max resistance - ball has to be at corresponding position
     void setMinResistance();
     void setMaxResistance();
 
+    //set sampleCount: number of measurements taken per reading
     void setSampleCount(int sampleCount);
+    //set sampleSelect: number of lowest measurements of which the average is calculated
     void setSampleSelect(int sampleSelect);
 
+    //
     double getMinResistance();
     double getMaxResistance();
 
